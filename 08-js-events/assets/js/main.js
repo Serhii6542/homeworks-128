5ab20c8a3f791afc1fc0fff69dc9cb9ab838ca99
Serhii6542 <plish4uk2111@gmail.com>
2024-02-02 22:39:39 +0200

    added 08-js-events

---
+++ 08-js-events/assets/js/main.js
@@ -0,0 +1,57 @@
+const div = document.createElement('div');
+div.classList = 'container';
+const divWrap = document.createElement('div');
+divWrap.classList = 'wrap';
+divWrap.id = 'wrap';
+divWrap.textContent = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus necessitatibus dolorum ratione sint aut illo.';
+div.prepend(divWrap)
+document.body.prepend(div);
+
+let editing = false;
+let text = '';
+let resizi = false;
+
+divWrap.addEventListener('mousedown', function(event){
+    resizi = true;
+    document.addEventListener('mousemove', editResizi);
+    document.addEventListener('mouseup',function(){
+        resizi = false;
+        document.removeEventListener('mousemove', editResizi);
+    })
+});
+
+function editResizi(event){
+    if(resizi){
+        divWrap.style.width = event.clientX - divWrap.offsetLeft + 'px';
+        divWrap.style.height = event.clientY - divWrap.offsetTop + 'px';
+    }
+}
+
+document.addEventListener('keydown', function(event){
+    if (event.ctrlKey && event.code === 'KeyE') {
+        event.preventDefault();
+        if(!editing){
+            editing = true;
+            text = document.getElementById('wrap').innerText;
+            const textarea = document.createElement('textarea');
+            textarea.id = 'textTeria';
+            textarea.classList = 'textarea';
+            textarea.value = text;
+            document.getElementById('wrap').replaceWith(textarea);
+            textarea.focus()
+        }
+    }
+});
+
+document.addEventListener('keydown', function(event){
+    if(event.ctrlKey && event.code === 'KeyS'){
+        event.preventDefault();
+        if(editing){
+            editing = false;
+            text = document.getElementById('textTeria').value;
+            divWrap.textContent = text;
+            document.getElementById('textTeria').replaceWith(divWrap);
+        }
+    }
+})
+
